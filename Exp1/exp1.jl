88973099b4f9c0f3067de9dd6e9ac8f1c86efb6f
Leonardo Isao Komura <leokomura@gmail.com>
2022-04-22 21:27:47 -0300

    exp1inc

---
+++ Exp1/exp1.jl
@@ -0,0 +1,32 @@
+using DSP
+using Plots
+
+Ω = 2pi*500;
+τ = 0.5;
+f_a = 40000;
+t = 0:1/f_a:2;
+x0 = (sin.(Ω.*t).^3).*exp.(-t./τ);
+#plot(t, x0)
+
+#revisar amplitude do ruído
+wgn = randn((2*f_a)+1);
+wgn_10dB = wgn ./ (10^(1/4));
+x = x0 .+ wgn_10dB;
+#display(plot(t, x))
+
+n = 0:1:100;
+h = 0.1.*sinc.(0.1.*(n.-50));
+#display(plot(n, h))
+hz = PolynomialRatio(h, [1]);
+ω = range(0, π, length=500);
+H = freqz(hz, ω);
+#display(plot(ω/π, abs.(H)))
+
+y = filt(hz, x)
+y0 = filt(hz, x0)
+n1 = 0:1:80000
+#fazer esse plot no msm gráfico
+#display(plot(n1, y0))
+#display(plot(n1, y))
+
+#e)
